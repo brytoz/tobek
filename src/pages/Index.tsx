2c6f6ce64438b68ff130826bc64817f1e3045f91
MAC <brytozee@yahoo.com>
2025-08-04 17:05:18 +0100

    modifieif

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,16 @@
 import Nav from "../components/Nav";
-// import Head from "../components/home/Head"; 
-import Test from "../components/home/Test"; 
 import Why from "../components/home/Why";
 import ContactComp from "../components/home/ContactComp";
 import Footer from "../components/Footer";
 import ProductsCards from "../components/home/ProductsCards";
+import HeroCarousel from "../components/home/carousal/HeroCarousel";
 
 const Index = () => {
   return (
     <div className="m-0">
       <Nav />
-      {/* <Head /> */}
-      <Test />
-
+   
+      <HeroCarousel />
 
       <Why />
 
